4cd91b40800d2ed0ff792f006765cb3e63e95c3e
prabhasha-p <74059196+prabhasha-p@users.noreply.github.com>
2021-03-18 07:14:10 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,4 +1,4 @@
-bot_token = "1488727769:AAElRU7yS309GP1t54F635ASDF"
+bot_token = "1676779247:AAE575MIxfFrf-00-urkL-mgC10IJwtgyxE"
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
-sudo_chat_id = -1001485876964
-owner_id = 1243703097
+sudo_chat_id = -1001461928414
+owner_id = 1254831277
